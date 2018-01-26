ac1fd1fea39c167211067e528405ba33e7109f03
Nayrb93 <bryan.comeche@gmail.com>
2018-01-26 10:53:59 +0100

    Test Laravol

--- grepBryan/resources/views/welcome.blade.php
+++ grepBryan/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravol
                 </div>
 
                 <div class="links">
