0ad17005695ff231b82498adb51489882a4192a2
cyberbat <richard.q.dayao@gmail.com>
2018-01-26 11:42:33 +0100

    Revert "Merge pull request #4 from Nayrb93/putaPanV2"
    This reverts commit 1baac2f449a1bbb797b962a74af557316b1d65f3, reversing
    changes made to 860a86f127444e4f20636b2814cfc7466716ed01.

--- grepBryan/resources/views/welcome.blade.php
+++ grepBryan/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravol-FDP-putaPV2
+                    Laravol-FDP-putaPAN
                 </div>
 
                 <div class="links">
