78f5457133cafc44315b801e1aa5ee51ff6092a6
Nayrb93 <bryan.comeche@gmail.com>
2018-01-26 10:57:46 +0100

    test branche

--- grepBryan/resources/views/welcome.blade.php
+++ grepBryan/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravol
+                    Laravol-testbranche
                 </div>
 
                 <div class="links">
