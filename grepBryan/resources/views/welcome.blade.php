27ff258a1bd74fcf3e309442699b182bb8acaf5d
cyberbat <richard.q.dayao@gmail.com>
2018-01-26 11:11:45 +0100

    modif
    modif

--- grepBryan/resources/views/welcome.blade.php
+++ grepBryan/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravol-FDP
+                    Laravol-FDP-putaPAN
                 </div>
 
                 <div class="links">
